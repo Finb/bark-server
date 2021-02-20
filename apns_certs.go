6bc0e96a85198097e20e2727956f332e16f7c1ef
mritd <mritd@linux.com>
2021-02-20 19:48:12 +0800

    feat(apns): add private key
    add private key
    
    Signed-off-by: mritd <mritd@linux.com>

--- apns_certs.go
+++ apns_certs.go
@@ -1,5 +1,13 @@
 package main
 
+var apnsPrivateKey = `-----BEGIN PRIVATE KEY-----
+MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQg4vtC3g5L5HgKGJ2+
+T1eA0tOivREvEAY2g+juRXJkYL2gCgYIKoZIzj0DAQehRANCAASmOs3JkSyoGEWZ
+sUGxFs/4pw1rIlSV2IC19M8u3G5kq36upOwyFWj9Gi3Ejc9d3sC7+SHRqXrEAJow
+8/7tRpV+
+-----END PRIVATE KEY-----
+`
+
 var apnsCAs = []string{
 	// AppleComputerRootCertificate.cer
 	`-----BEGIN CERTIFICATE-----
