473ee98596b4ef4f760fd62f5bf78c5b41fd5e79
Fin <heyfiniks@gmail.com>
2021-07-02 16:04:59 +0800

    fix: #66

--- route_auth.go
+++ route_auth.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	fiberbasicauth "github.com/gofiber/fiber/v2/middleware/basicauth"
 	"github.com/mritd/logger"
+	"strings"
 )
 
 func routerAuth(user, passwd string, router *fiber.App) {
@@ -13,12 +14,16 @@ func routerAuth(user, passwd string, router *fiber.App) {
 			Users: map[string]string{user: passwd},
 			Realm: "Coffee Time",
 			Unauthorized: func(c *fiber.Ctx) error {
+				authFreeRouters := []string{"/ping", "/register", "/healthz"}
+				for _, item := range authFreeRouters{
+					if strings.HasPrefix(c.Path(), item) {
+						return c.Next()
+					}
+				}
 				return c.Status(418).SendString("I'm a teapot")
 			},
 		})
-		router.Use("/push", basicAuth)
-		router.Use("/:device_key/:body", basicAuth)
-		router.Use("/:device_key/:title/:body", basicAuth)
-		router.Use("/:device_key/:category/:title/:body", basicAuth)
+
+		router.Use("/+", basicAuth)
 	}
 }
