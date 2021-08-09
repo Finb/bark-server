c95f5fce83f7f4c6820c09e989f8ddb0a36d8447
Fin <heyfiniks@gmail.com>
2021-08-09 14:01:19 +0800

    修复默认data路径参数

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ func main() {
 				Name:    "data",
 				Usage:   "Server data storage dir",
 				EnvVars: []string{"BARK_SERVER_DATA_DIR"},
-				Value:   "/Users/huangfeng/Desktop",
+				Value:   "/Users",
 			},
 			&cli.StringFlag{
 				Name:    "cert",
