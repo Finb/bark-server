76234044ceee7c042c7e528ddad7369cb0df81ff
citizen233 <mengrenxiong@gmail.com>
2022-06-20 10:01:46 +0800

    feat: 优化 时区设置

---
+++ deploy/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+set -e
+
+if [ ! -z "$TZ" ];then
+	cp "/usr/share/zoneinfo/$TZ" /etc/localtime
+	echo "$TZ" > /etc/timezone
+else
+	cp "/usr/share/zoneinfo/Asia/Shanghai" /etc/localtime
+	echo "Asia/Shanghai" > /etc/timezone
+fi
+
+/usr/local/bin/bark-server
+
+sleep infinity
