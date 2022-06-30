90484cc1fcf009691bf2a3c92f77fd9319011134
kovacs <mritd@linux.com>
2022-06-30 17:06:08 +0800

    chore(docker): fix ci build failed
    fix ci build failed
    
    Signed-off-by: kovacs <mritd@linux.com>

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
-#
+
 set -e
 
-if [ ! -z "$TZ" ];then
-	cp "/usr/share/zoneinfo/$TZ" /etc/localtime
-	echo "$TZ" > /etc/timezone
-else
-	cp "/usr/share/zoneinfo/Asia/Shanghai" /etc/localtime
-	echo "Asia/Shanghai" > /etc/timezone
-fi
+ln -sf "/usr/share/zoneinfo/${TZ}" /etc/localtime
+echo "${TZ}" > /etc/timezone
 
 exec "$@"
